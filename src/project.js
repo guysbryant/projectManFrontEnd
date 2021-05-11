12c9899cd3c0876271e5b9e6a5fda1602a117335
guysbryant <guy.s.bryant@gmail.com>
2021-05-10 22:11:33 -0400

    change render to renderProject, add all and findById static methods, add renderProjectTasks instance methods

--- src/project.js
+++ src/project.js
@@ -4,9 +4,13 @@ class Project {
         this.description = project.attributes.description;
         this.name = project.attributes.name;
         this.tasks = project.attributes.tasks.map(task => new Task(task));
+        Project.all.push(this);
     }
 
-    render(){
+    static all = [];
+    static findById = (projectId) => Project.all.find(project => project.id == projectId);
+
+    renderProject(){
         const projectCardList = document.querySelector("#master-card-list");
         const projectCard = document.createElement("div");
         projectCardList.appendChild(projectCard);
@@ -18,5 +22,13 @@ class Project {
         const description = document.createElement("h3");
         projectCard.appendChild(description);
         description.innerText = `${this.description}`;
+        projectCard.addEventListener("click", () =>{
+            let project = Project.findById(projectCard.id);
+            project.renderProjectTasks();
+        });
+    }
+
+    renderProjectTasks(){
+        debugger;
     }
 }
