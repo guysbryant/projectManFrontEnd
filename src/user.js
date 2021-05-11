fe373991f9d0e68c7536342b4df05e6cb81cfe77
guysbryant <guy.s.bryant@gmail.com>
2021-05-10 21:11:59 -0400

    add user class

---
+++ src/user.js
@@ -0,0 +1,17 @@
+class User{
+    constructor(user){
+        this.id = user.id;
+        this.name = user.name;
+    }
+
+    render(){
+        const userCardList = document.querySelector("#master-card-list");
+        const userCard = document.createElement("div");
+        userCardList.appendChild(userCard);
+        userCard.id = this.id;
+        userCard.className = "user-card";
+        const name = document.createElement("h2");
+        userCard.appendChild(name);
+        name.innerText = `${this.name}`;
+    }
+}
